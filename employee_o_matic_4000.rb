2891cbf0320fa8d430cc3df7be42ddfaeef5fd21
MrPranklin <filip.pranklin@gmail.com>
2018-10-17 14:48:40 +0200

    Style fixes

--- employee_o_matic_4000.rb
+++ employee_o_matic_4000.rb
@@ -7,11 +7,11 @@ class Employee
     @id = id
   end
 
-  def lastname
-    @full_name.split(' ', 2).last       #split into 2 substrings, first and last name, take the latter
+  def last_name
+    @full_name.split(' ', 2).last # split into 2 substrings, first and last name, take the latter
   end
 
-  def firstname
+  def first_name
     @full_name.split(' ').first
   end
 end
@@ -34,14 +34,14 @@ class OfficeManager < Employee
 end
 
 def add_employee(employees)               #      adds an employee to array
-  puts '[Add an employee]'                #param +employees+ array of employees
+  puts '[Add an employee]'                # param +employees+ array of employees
   print 'Full name: '
   full_name = gets.chomp
   print 'ID: '
   id = gets.chomp
 
   position = nil
-  loop do                                 #ask for input until correct format is entered
+  loop do                                 # ask for input until correct format is entered
     print 'Is this person an [e]mployee, [p]rogrammer or an [o]ffice manager?'
     position = get_action
     case position
@@ -51,15 +51,16 @@ def add_employee(employees)               #      adds an employee to array
     end
   end
 
-  if position == 'e'
-      employee = Employee.new(full_name, id)
+  case position
+  when 'e' then
+    employee = Employee.new(full_name, id)
 
-  elsif position == 'p'
+  when 'p' then
     print 'Languages: '
     languages = gets.chomp
     employee = Programmer.new(full_name, id, languages)
 
-  elsif position == 'o'
+  when 'o' then
     print 'Office: '
     office = gets.chomp
     employee = OfficeManager.new(full_name, id, office)
@@ -68,76 +69,74 @@ def add_employee(employees)               #      adds an employee to array
   employees << employee
 end
 
-def edit_employee(employees)                #      edits employees
-  puts 'Who do you want to edit?'           #param +employees+ array of existing employees
-  print 'Full name: '
+def edit_employee(employees)
+  puts 'Who do you want to edit?' #       edits employees
+  print 'Full name: ' # param +employees+ array of existing employees
   full_name = gets.chomp
   print 'ID: '
   id = gets.chomp
 
-  employee = employees.find{|employee| employee.full_name == full_name && employee.id == id}
-  if employee != nil
-      puts '[Selected employee]'
-      puts "#{employee.full_name}, #{employee.id}"
-      print 'New full name: '
-      new_full_name = gets.chomp
-      print 'New ID: '
-      new_id = gets.chomp
-      if employee.is_a?(OfficeManager)
-        print 'New office: '
-        new_office = gets.chomp
-        if new_office.length > 0
-          employee.office = new_office
-        end
-      elsif employee.is_a?(Programmer)
-        print 'New languages: '
-        new_languages = gets.chomp
-        if new_languages.length > 0
-          employee.languages = new_languages
-        end
-      end
-      employee.full_name = new_full_name
-      employee.id = new_id
-      return                                #if an edit succeeded, exit method
+  employee = employees.find do |employee|
+    employee.full_name == full_name && employee.id == id
   end
-    puts 'No matching employee found.'
-    edit_employee(employees)                #else, call method once again
-     end                                    #it could be done using loop do...end through the whole method,
-                                            #but it looks weird, I'm not aware if it could cause any problems
+  unless employee.nil?
+    puts '[Selected employee]'
+    puts "#{employee.full_name}, #{employee.id}"
+    print 'New full name: '
+    new_full_name = gets.chomp
+    print 'New ID: '
+    new_id = gets.chomp
+    if employee.is_a?(OfficeManager)
+      print 'New office: '
+      new_office = gets.chomp
+      employee.office = new_office unless new_office.empty?
+    elsif employee.is_a?(Programmer)
+      print 'New languages: '
+      new_languages = gets.chomp
+      employee.languages = new_languages unless new_languages.empty?
+    end
+    employee.full_name = new_full_name
+    employee.id = new_id
+    return # if an edit succeeded, exit method
+  end
+  puts 'No matching employee found.'
+  edit_employee(employees)                  # else, call method once again
+     end                                    # it could be done using loop do...end through the whole method,
+                                            # but it looks weird, I'm not aware if it could cause any problems
 
 def sort_employees(employees, argument)
   employees.sort_by do |employee|
     case argument
-    when 'f' then employee.firstname
-    when 'l' then employee.lastname
+    when 'f' then employee.first_name
+    when 'l' then employee.last_name
     end
   end
 end
 
 def get_action
-  return gets.downcase[0]
+  gets.downcase[0]
 end
 
 def print_employee(employee, argument)
   case argument
-  when 'f' then print "#{employee.firstname} #{employee.lastname}, #{employee.id}"          #      prints an employee in "first/last name, last/first name, id" format
-  when 'l' then print "#{employee.lastname}, #{employee.firstname}, #{employee.id}"         #param +employee+ employee with according hash you want printed
-  end                                                                                           #param +argument+ if 'f' then first way of printing is used, if 'l' the other way is used
+  when 'f' then print "#{employee.first_name} #{employee.last_name}, #{employee.id}"          #       prints an employee in "first/last name, last/first name, id" format
+  when 'l' then print "#{employee.last_name}, #{employee.first_name}, #{employee.id}"         # param +employee+ employee with according hash you want printed
+  end                                                                                           # param +argument+ if 'f' then first way of printing is used, if 'l' the other way is used
 
   case employee
-  when OfficeManager then puts ", office: #{employee.office}"
-  when Programmer then puts ", language: #{employee.languages}"
+  when OfficeManager then puts ", #{employee.office}"
+  when Programmer then puts ", #{employee.languages}"
   when Employee then print "\n"
   end
 end
 
 def view_employees(employees)
   argument = nil
-  loop do                                                             #ask for input until correct format is entered
+  loop do                                                             # ask for input until correct format is entered
     puts 'Sort by [f]irst name or [l]ast name? '
     argument = get_action
-    break if argument == 'f' || argument == 'l'                       #      prints all employees
-  end                                                                 #param +employees+ array of employees you want to print
+    break if argument == 'f' || argument == 'l'                       #       prints all employees
+  end                                                                 # param +employees+ array of employees you want to print
   puts '[List of employees]'
   sort_employees(employees, argument).each do |employee|
     print_employee(employee, argument)
@@ -158,7 +157,7 @@ def print_action_help
   puts 'q - to quit the program'
 end
 
-puts "Employee-o-matic 4000"
+puts 'Employee-o-matic 4000'
 
 employees = []
 
