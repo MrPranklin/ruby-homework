5564a51bb11a59b4d1a878a7253d406a838e1f88
MrPranklin <filip.pranklin@gmail.com>
2018-10-26 10:48:41 +0200

    Made it pretty

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+/* app/assets/javascript/application.js */
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
