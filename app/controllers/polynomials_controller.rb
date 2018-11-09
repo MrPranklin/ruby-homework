ac4ea3849ea9d96e14e969ae8b984887d1df1f62
MrPranklin <filip.pranklin@gmail.com>
2018-11-09 12:49:28 +0100

    Finished chapter

--- app/controllers/polynomials_controller.rb
+++ app/controllers/polynomials_controller.rb
@@ -1,8 +1,10 @@
-#app/controllers/polynomials_controller.rb
+# app/controllers/polynomials_controller.rb
 
 class PolynomialsController < ApplicationController
   def show
-
+    @solution = params[:id]
+    @solution = Base64.decode64(@solution)
+    @solution = Marshal.restore(@solution)
   end
 
   def new
@@ -10,6 +12,28 @@ class PolynomialsController < ApplicationController
   end
 
   def create
+    a = params[:a].to_i
+    b = params[:b].to_i
+    c = params[:c].to_i
+    d = params[:d].to_i
 
+    solution = solve(a, b, c, d)
+    solution = Marshal.dump(solution)
+    solution = Base64.encode64(solution)
+
+    redirect_to "/polynomials/#{solution}"
+  end
+
+  def solve(a, b, c, d)
+    require 'cmath'
+
+    a = a.to_r
+    b = b.to_r
+    c = (c - d).to_r
+
+    first = -1 * b
+    second = CMath.sqrt(b**2 - 4 * a * c) / (2 * a)
+
+    [first - second, first + second]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  root 'landing#index'
+
   resources :comments
   resources :posts
   resources :authors
-  get 'landing/index'
-  
-  root 'landing#index'
+
+  #get 'landing/index' #not sure if this is necessary
+
+  get 'polynomials/new', to: 'polynomials#new'
+  get 'polynomials/:id', to: 'polynomials#show'
+  post 'polynomials', to: 'polynomials#create'
 end
