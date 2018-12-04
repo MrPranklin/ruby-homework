01e76dda38f486d33e7f29298d6e3ca5dc454aa6
MrPranklin <filip.pranklin@gmail.com>
2018-12-04 20:17:54 +0100

    Some advances in 4th assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  root 'landing#index'
+
   resources :comments
   resources :posts
   resources :authors
-  get 'landing/index'
-  
-  root 'landing#index'
+
+  #get 'landing/index' #not sure if this is necessary
+
+  get 'polynomials/new', to: 'polynomials#new'
+  get 'polynomials/:id', to: 'polynomials#show'
+  post 'polynomials', to: 'polynomials#create'
+
+  get 'captcha/new', to: 'captcha#new'
+  get 'captcha/:captcha', to: 'captcha#show'
+  post 'captcha', to: 'captcha#create'
 end
