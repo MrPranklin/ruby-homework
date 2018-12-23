78b7a4d8c7024a976cd7b80d3ce7057854fbe9ac
MrPranklin <filip.pranklin@gmail.com>
2018-12-23 17:30:43 +0100

    Installed annotate gem, annotated

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  post_id    :integer
+#  author_id  :integer
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ApplicationRecord
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  author_id  :integer
+#  content    :text
+#  published  :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
