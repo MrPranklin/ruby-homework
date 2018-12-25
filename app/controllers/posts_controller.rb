2a29c9dd2aec609ec0952c2180c291919d4c04e8
MrPranklin <filip.pranklin@gmail.com>
2018-12-25 22:24:15 +0100

    Tried to change input for post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:author_id, :content, :published)
+      params.require(:post).permit(:author_id, :title, :sub_reddit_id, :content, :published)
     end
 end
