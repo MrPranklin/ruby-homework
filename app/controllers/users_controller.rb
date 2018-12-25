df5f665bde1b6e4f6e0b7f256daf7732728472c3
MrPranklin <filip.pranklin@gmail.com>
2018-12-25 22:58:45 +0100

    Add mock routes for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def index
-    
+    @users = User.all
+  end
+
+  def show
+    @user = params[:id]
+  end
+
+  def new
+    @user = User.new
   end
 end
